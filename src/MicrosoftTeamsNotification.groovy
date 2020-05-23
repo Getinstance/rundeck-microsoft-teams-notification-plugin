9b6373d1c63bc986c528726e46bf41949f3ff3a9
Raul Dias <raul.getinstance@gmail.com>
2020-05-23 01:05:59 -0300

    Update MicrosoftTeamsNotification.groovy

--- src/MicrosoftTeamsNotification.groovy
+++ src/MicrosoftTeamsNotification.groovy
@@ -1,80 +1,83 @@
-import com.dtolabs.rundeck.plugins.notification.NotificationPlugin
-import groovy.json.JsonOutput
+  import com.dtolabs.rundeck.plugins.notification.NotificationPlugin
+  import groovy.json.JsonOutput
 
 
-rundeckPlugin(NotificationPlugin){
-    title="Microsoft Teams notification Plugin"
-    description="Allows to set up notification for Microsoft Teams chats for a channel, via Webhook URL. To use it you will have to obtain webhook for your channel first and setit up."
+  rundeckPlugin(NotificationPlugin){
+      title="Microsoft Teams notification Plugin"
+      description="Allows to set up notification for Microsoft Teams chats for a channel, via Webhook URL. To use it you will have to obtain webhook for your channel first and setit up."
 
-    configuration{
-        webhook_url title:"Webhook URL", required: true, type:"String", description:"You may find it in Microsoft Teams Channel user interfaces by using Incomming Webhook connector via:  Channel Name -> Connectors -> Incomming Webhook"
-    }
+      //configuration{
+      //  webhook_url title:"Webhook URL", required: true, type:"String", description:"You may find it in Microsoft Teams Channel user interfaces by using Incomming Webhook connector via:  Channel Name -> Connectors -> Incomming Webhook"
+      //}
+      // ${configuration.webhook_url}
 
-    onstart {
-        type = "START"
-        color = "696969"
-        json_payload = JsonOutput.toJson([
-            title: "[${type}] Rundeck Job Notification - ${execution.project}:${execution.job.group}:${execution.job.name}",
-            summary: "Rundeck Job Notification",
-            text: "$job id: #${execution.job.id}, job project: ${execution.project}, job group: ${execution.job.group}, job name: ${execution.job.name}, job description: ${execution.job.description}, execution id: #${execution.id}, execution status: ${execution.status}, execution started at: ${execution.dateStarted}",
-            themeColor: "${color}",
-            potentialAction: [
-                [
-                    "@context": "http://schema.org",
-                    "@type": "ViewAction",
-                    name: "Seed job execution",
-                    target: ["${execution.href}"]
-                ]
-            ]
-        ])
-        process = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json_payload}' ${configuration.webhook_url}" ].execute().text
+      onstart { Map execution, Map configuration ->
+          type = "START"
+          color = "696969"
+          appname = [execution.context.option.Appname, execution.context.option.App_name].findAll().join(' ')
+          json_payload = JsonOutput.toJson([
+              title: "${execution.job.name} - ${execution.user} iniciou o deploy de ${appname}",
+              summary: "${execution.job.name}",
+              text: "${execution.context.option.url_nexus} foi o arquivo enviado",
+              themeColor: "${color}",
+              potentialAction: [
+                  [
+                      "@context": "http://schema.org",
+                      "@type": "ViewAction",
+                      name: "Olha o job aqui!",
+                      target: ["${execution.href}"]
+                  ]
+              ]
+          ])
+          process = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json_payload}' https://outlook.office.com/webhook/91744d32-9381-4748-bf35-4a3b3ba211a5@5a86b3fb-4213-49cd-b4d6-be91482ad3c0/IncomingWebhook/4bcf50721c1e423184279f44e29fbc24/4955d7e7-9d00-474a-b168-0c2e54f84f7e" ].execute().text
 
-        return true
-    }
+          return true
+      }
 
-    onfailure {
-        type = "FAILURE"
-        color = "E81123"
-        //Single argument, the configuration properties are available automatically
-        json_payload = JsonOutput.toJson([
-            title: "[${type}] Rundeck Job Notification - ${execution.project}:${execution.job.group}:${execution.job.name}",
-            summary: "Rundeck Job Notification",
-            text: "job id: #${execution.job.id}, job project: ${execution.job.project}, job group: ${execution.job.group}, job name: ${execution.job.name}, job description: ${execution.job.description}, execution id: #${execution.id}, execution status: ${execution.status}, execution started at: ${execution.dateStarted}, execution ended at: ${execution.dateEnded}",
-            themeColor: "${color}",
-            potentialAction: [
-                [
-                    "@context": "http://schema.org",
-                    "@type": "ViewAction",
-                    name: "Seed job execution",
-                    target: ["${execution.href}"]
-                ]
-            ]
-        ])
-        process = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json_payload}' ${configuration.webhook_url}" ].execute().text
+      onfailure { Map execution, Map configuration ->
+          type = "FAILURE"
+          color = "E81123"
+          appname = [execution.context.option.Appname, execution.context.option.App_name].findAll().join(' ')
+          //Single argument, the configuration properties are available automatically
+          json_payload = JsonOutput.toJson([
+              title: "${execution.job.name} - ${execution.user} quebrou o deploy de ${appname}",
+              summary: "${execution.job.name}",
+              text: "${execution.context.option.url_nexus} foi o arquivo enviado",
+              themeColor: "${color}",
+              potentialAction: [
+                  [
+                      "@context": "http://schema.org",
+                      "@type": "ViewAction",
+                      name: "Olha o job aqui!",
+                      target: ["${execution.href}"]
+                  ]
+              ]
+          ])
+          process = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json_payload}' https://outlook.office.com/webhook/91744d32-9381-4748-bf35-4a3b3ba211a5@5a86b3fb-4213-49cd-b4d6-be91482ad3c0/IncomingWebhook/4bcf50721c1e423184279f44e29fbc24/4955d7e7-9d00-474a-b168-0c2e54f84f7e" ].execute().text
 
-        return true
-    }
+          return true
+      }
 
-    onsuccess {
-        type = "SUCCESS"
-        color = "228B22"
-        //with no args, there is a "configuration" and an "execution" variable in the context
-        json_payload = JsonOutput.toJson([
-            title: "Rundeck Job Notification",
-            title: "[${type}] Rundeck Job Notification - ${execution.project}:${execution.job.group}:${execution.job.name}",
-            text: "job id: #${execution.job.id}, job project: ${execution.job.project}, job group: ${execution.job.group}, job name: ${execution.job.name}, job description: ${execution.job.description}, execution id: #${execution.id}, execution status: ${execution.status}, execution started at: ${execution.dateStarted}, execution ended at: ${execution.dateEnded}",
-            themeColor: "${color}",
-            potentialAction: [
-                [
-                    "@context": "http://schema.org",
-                    "@type": "ViewAction",
-                    name: "Seed job execution",
-                    target: ["${execution.href}"]
-                ]
-            ]
-        ])
-        process = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json_payload}' ${configuration.webhook_url}" ].execute().text
+      onsuccess { Map execution, Map configuration ->
+          type = "SUCCESS"
+          color = "228B22"
+          appname = [execution.context.option.Appname, execution.context.option.App_name].findAll().join(' ')
+          //with no args, there is a "configuration" and an "execution" variable in the context
+          json_payload = JsonOutput.toJson([
+              title: "${execution.job.name} - ${execution.user} conseguiu fazer o deploy de ${appname}",
+              text: "${execution.context.option.url_nexus} foi o arquivo enviado",
+              themeColor: "${color}",
+              potentialAction: [
+                  [
+                      "@context": "http://schema.org",
+                      "@type": "ViewAction",
+                      name: "Olha o job aqui!",
+                      target: ["${execution.href}"]
+                  ]
+              ]
+          ])
+          process = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json_payload}' https://outlook.office.com/webhook/91744d32-9381-4748-bf35-4a3b3ba211a5@5a86b3fb-4213-49cd-b4d6-be91482ad3c0/IncomingWebhook/4bcf50721c1e423184279f44e29fbc24/4955d7e7-9d00-474a-b168-0c2e54f84f7e" ].execute().text
 
-        return true
-    }
-}
+          return true
+      }
+  } 
